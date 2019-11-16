eae5f39b0899c630a27a842a31cc006f744a2cbe
Ilia Sahaidak <ilsaha@pascalium.com>
2019-11-16 11:00:12 +0200

    Initial Commit

---
+++ homework-5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework-5
+//
+//  Created by Ilia Sahaidak on 16.11.2019.
+//  Copyright © 2019 Ilia Sahaidak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
